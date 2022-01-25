decb31d5fc6554fa689f70202e2e78d120b260a7
Shane <Shane@DESKTOP-Q1DOOLC.mshome.net>
2022-01-25 15:05:54 -0600

    Testing eclipse/changed spacing

--- rubik/check.py
+++ rubik/check.py
@@ -2,9 +2,20 @@ import rubik.cube as rubik
 
 def _check(parms):
     result={}
-    encodedCube = parms.get('cube',None)       
+    encodedCube = parms.get('cube',None)  
+         
+         
+         
     if(encodedCube == None):
         result['status'] = 'error: xxx'
+        
+        
+        
     else:
         result['status'] = 'ok'
+        
+        
+        
+        
+        
     return result

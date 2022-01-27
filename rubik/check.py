f239e2ef1bf1b58a723580a8af56258da708eb1c
Shane <Shane@DESKTOP-Q1DOOLC.mshome.net>
2022-01-26 20:05:58 -0600

    Updated tests

--- rubik/check.py
+++ rubik/check.py
@@ -3,13 +3,29 @@ import rubik.cube as rubik
 def _check(parms):
     result={}
     encodedCube = parms.get('cube',None)  
-         
+    numOfUniqueChars = set(encodedCube)     
          
          
     if(encodedCube == None):
         result['status'] = 'error: xxx'
         
+    elif(len(encodedCube) < 54):   
+        result['status'] = 'error: cube is less than the required length of 54'
         
+    elif(len(encodedCube) > 54):   
+        result['status'] = 'error: cube is greater than the required length of 54'
+        
+    elif(' ' in encodedCube ):
+        result['status'] = 'error: cube has white space'  
+        
+    elif(len(numOfUniqueChars) > 6 ):
+        result['status'] = 'error: cube requires less unique chars (exactly 6 are required)'
+        
+    elif(len(numOfUniqueChars) < 6 ):
+        result['status'] = 'error: cube requires more unique chars (exactly 6 are required)' 
+        
+    elif(' ' in encodedCube ):
+        result['status'] = 'error: cube has white space'  
         
     else:
         result['status'] = 'ok'
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -9,5 +9,75 @@ class CheckTest(TestCase):
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
+        ##Check to see that output is equal to 'ok' in check() function
         self.assertEqual(status, 'ok')
+        
+        
+        #Come back and add tests that check to ensure middle pieces are separate, as well as edge pieces
+        
+        
+        
+        
 
+    
+    #Length is less than 54
+    #Does not pass\returns error msg    
+    def test_check_LengthIsLessThan54(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbrrrrrrrrggggggggoooooooooyyyyyyyywwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is less than the required length of 54')
+        
+    
+    
+    #Length is greater than 54 
+    #Does not pass\returns error msg
+    def test_check_LengthIsGreaterThan54(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is greater than the required length of 54')
+    
+    
+    #Given string has whitespace    
+    #Does not pass\returns error msg  
+    def test_check_HasWhiteSpace(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrr rgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube has white space' )   
+        
+        
+    #Number of unique values doesn't equal six
+    #Does not pass\returns error msg
+    def test_check_HasLessThanSixUniqueValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbbbbbbbbgggggggggoooooooooyyyyyyyyyyyyyyyyyyyy'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube requires more unique chars (exactly 6 are required)')
+    
+     
+    #Has more than 6 unique characters in string
+    #Does not pass\returns error msg 
+    def test_check_HasMoreThanSixUniqueValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbvvbbbbbbbgggggggggopoooooooyyyyyyyyyyyyyyyyyyxx'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube requires less unique chars (exactly 6 are required)')
+     
+     
+    
+        
+        
+        
+           

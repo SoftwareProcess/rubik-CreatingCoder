96180288ea795a39f5fb382f544078fab98d29ea
Shane <Shane@DESKTOP-Q1DOOLC.mshome.net>
2022-01-30 21:26:30 -0600

    Added more test cases

--- rubik/check.py
+++ rubik/check.py
@@ -1,42 +1,96 @@
 #import rubik.cube as rubik
 
+#Shane Morgan
+#szm0152@auburn.edu
+#Behold, my if-elif-else statements
+
 def _check(parms):
-    forbiddenchars = '`~!@#$%^&*()-_=+[]{}\|";:?/.>,<\''
+    #String that has chars 
+    forbiddenChars = '`~!@#$%^&*()-_=+[]{}\|";:?/.>,<\''
     result={}
-    encodedCube = parms.get('cube',None)  
-    numOfUniqueChars = set(encodedCube)     
-         
-         
+    encodedCube = parms.get('cube', None)
+    cubeType = type(encodedCube)
+    
+    #Sets some variables under the pretense that the cube != None & encodedCube type == string
+    if((encodedCube != None) & (cubeType == str)):                                             
+        uniqueChars = set(encodedCube)
+        uniqueCharsList = list(uniqueChars)
+    
+    #Ensures cube isn't None    
     if(encodedCube == None):
-        result['status'] = 'error: xxx'
+        result['status'] = 'error: cube is missing'
         
+    #Checks if the encodedCube is a str value
+    elif(cubeType != str):
+        result['status'] = 'error: cube must be a String' 
+    
+    #Checks for empty str          
+    elif(encodedCube == ''):
+        result['status'] = 'error: cube cannot be an empty string'
+    
+    #Checks to ensure the cube string is not too small    
     elif(len(encodedCube) < 54):   
         result['status'] = 'error: cube is less than the required length of 54'
-        
+    
+    #Checks to ensure the cube string is not too large      
     elif(len(encodedCube) > 54):   
         result['status'] = 'error: cube is greater than the required length of 54'
-        
+    
+    #Checks for white space   
     elif(' ' in encodedCube ):
         result['status'] = 'error: cube has white space'  
-        
-    elif(len(numOfUniqueChars) > 6 ):
+     
+    #Checks if there are less than 6 unique chars   
+    elif(len(uniqueChars) > 6 ):
         result['status'] = 'error: cube requires less unique chars (exactly 6 are required)'
-        
-    elif(len(numOfUniqueChars) < 6 ):
-        result['status'] = 'error: cube requires more unique chars (exactly 6 are required)' 
-        
-    elif(' ' in encodedCube ):
-        result['status'] = 'error: cube has white space'
     
-    elif any(c in forbiddenchars for c in encodedCube):
+    #Checks if there are more than 6 unique chars      
+    elif(len(uniqueChars) < 6 ):
+        result['status'] = 'error: cube requires more unique chars (exactly 6 are required)' 
+    
+    #Checks cube for forbidden chars (only permitted chars are a-z, A-Z, 0-9)
+    elif any(c in forbiddenChars for c in encodedCube):
         result['status'] = 'error: cube cannot contain chars including:`~!@#$%^&*()-_=+[]{}\|";:?/.>,<\''  
-      
+
+         
+    #Checks center for first face to ensure there are no matching 
+    elif(encodedCube[4] in (encodedCube[13], encodedCube[22], encodedCube[31], encodedCube[40], encodedCube[49])):
+        result['status'] = 'error: cube must 6 unique centers'
         
+    elif(encodedCube[13] in (encodedCube[22], encodedCube[31], encodedCube[40], encodedCube[49])):
+        result['status'] = 'error: cube must 6 unique centers'
+        
+    elif(encodedCube[22] in (encodedCube[31], encodedCube[40], encodedCube[49])):
+        result['status'] = 'error: cube must 6 unique centers'
+        
+    elif(encodedCube[31] in (encodedCube[40], encodedCube[49])):
+        result['status'] = 'error: cube must 6 unique centers'
+        
+    elif(encodedCube[40] in (encodedCube[49])):
+        result['status'] = 'error: cube must 6 unique centers'
+          
+    #ensuring that there are 6 different chars and that each str contains 9 of each char
+    #Note: using the list method doesn't ensure a FIFO or FILO order, it instead randomizes it
+    elif(encodedCube.count(uniqueCharsList[0]) != 9 ):    
+        result['status'] = 'error: cube must have exactly 6 unique chars, and 9 of each unique chars' 
+         
+    elif(encodedCube.count(uniqueCharsList[1]) != 9 ):    
+        result['status'] = 'error: cube must have exactly 6 unique chars, and 9 of each unique chars' 
+    
+    elif(encodedCube.count(uniqueCharsList[2]) != 9 ):    
+        result['status'] = 'error: cube must have exactly 6 unique chars, and 9 of each unique chars' 
+        
+    elif(encodedCube.count(uniqueCharsList[3]) != 9 ):    
+        result['status'] = 'error: cube must have exactly 6 unique chars, and 9 of each unique chars' 
+        
+    elif(encodedCube.count(uniqueCharsList[4]) != 9 ):    
+        result['status'] = 'error: cube must have exactly 6 unique chars, and 9 of each unique chars' 
+        
+    elif(encodedCube.count(uniqueCharsList[5]) != 9 ):    
+        result['status'] = 'error: cube must have exactly 6 unique chars, and 9 of each unique chars' 
+    
+    #if an error isn't raised in one of the above if-elif statements, else returns status of 'ok'        
     else:
         result['status'] = 'ok'
-        
-        
-        
-        
         
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -3,13 +3,14 @@ import rubik.check as check
 
 class CheckTest(TestCase):
         
+     #############Start of Happy Tests########################   
+        
     def test_check_char_ShouldReturnOkOnSolvedCube(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        ##Check to see that output is equal to 'ok' in check() function
         self.assertEqual(status, 'ok')
         
         
@@ -19,7 +20,6 @@ class CheckTest(TestCase):
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        ##Check to see that output is equal to 'ok' in check() function
         self.assertEqual(status, 'ok')
         
     
@@ -29,7 +29,6 @@ class CheckTest(TestCase):
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        ##Check to see that output is equal to 'ok' in check() function
         self.assertEqual(status, 'ok')
         
         
@@ -39,31 +38,40 @@ class CheckTest(TestCase):
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        ##Check to see that output is equal to 'ok' in check() function
         self.assertEqual(status, 'ok')
-    
     
     
     def test_check_num_and_char_capitalised_ShouldReturnOkOnSolvedCube(self):
         parm = {'op':'check',
-                'cube':'111111111AAAAAAAAA222222222EEEEEEEEERRRRRRRRRGGGGGGGGG'}
+                'cube':'111111111eeeeeeeee222222222EEEEEEEEERRRRRRRRRGGGGGGGGG'}
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        ##Check to see that output is equal to 'ok' in check() function
-        self.assertEqual(status, 'ok')
-    
-    
+        self.assertEqual(status, 'ok') 
         
-        #Come back and add tests that check to ensure middle pieces are separate, as well as edge pieces
+          
         
+    ###############Start of Error Tests#################
         
-        
-        
+    def test_check_CubeIsNone(self):
+        parm = {'op':'check',
+                'cube': None}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is missing')   
 
     
-    #Length is less than 54
-    #Does not pass\returns error msg    
+    def test_check_CubeIsEmptyString(self):
+        parm = {'op':'check',
+                'cube': ''}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube cannot be an empty string')   
+    
+    
+    #Length is less than 54  
     def test_check_LengthIsLessThan54(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbrrrrrrrrggggggggoooooooooyyyyyyyywwwwwwww'}
@@ -75,7 +83,6 @@ class CheckTest(TestCase):
     
     
     #Length is greater than 54 
-    #Does not pass\returns error msg
     def test_check_LengthIsGreaterThan54(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwww'}
@@ -86,7 +93,6 @@ class CheckTest(TestCase):
     
     
     #Given string has whitespace    
-    #Does not pass\returns error msg  
     def test_check_HasWhiteSpace(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrr rgggggggggoooooooooyyyyyyyyywwwwwwwww'}
@@ -94,13 +100,79 @@ class CheckTest(TestCase):
         self.assertIn('status', result)
         status = result.get('status', None)
         self.assertEqual(status, 'error: cube has white space' )   
+    
         
+    #checks to ensure it has 6 unique chars, with 9 elements of each separate char  
+    def test_check_01_HasUnequalAmountOfElements(self):
+        parm = {'op':'check',
+                 #cube string has 10b, 8r, 9g, 9o, 9y, 9w
+                'cube':'bbbbbbbbrrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars')
+        
+    def test_check_02_HasUnequalAmountOfElements(self):
+        parm = {'op':'check',
+                 #cube string has 8b, 10r, 9g, 9o, 9y, 9w
+                'cube':'bbbbbbbbrrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars')
+        
+    def test_check_03_HasUnequalAmountOfElements(self):
+        parm = {'op':'check',
+                 #cube string has 9b, 8r, 10g, 9o, 9y, 9w
+                'cube':'bbbbbbbbbrrrrrrrrggggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars')
+
+    def test_check_03_HasUnequalAmountOfElementsWithNums(self):
+        parm = {'op':'check',
+                 #cube string has 9b, 9r, 8g, 10o, 9 1's, 9w
+                'cube':'bbbbbbbbbrrrrrrrrrggggggggoooooooooo111111111wwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars')
+        
+    def test_check_04_HasUnequalAmountOfElements(self):
+        parm = {'op':'check',
+                 #cube string has 9b, 9r, 9g, 8o, 10y, 9w
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggooooooooyyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars')        
+    
+    
+    def test_check_05_HasUnequalAmountOfElements(self):
+        parm = {'op':'check',
+                 #cube string has 9b, 9r, 9g, 9o, 8y, 10w
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyywwwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars')   
+        
+    def test_check_06_HasUnequalAmountOfElements(self):
+        parm = {'op':'check',
+                 #cube string has 10b, 9r, 9g, 9o, 9y, 8w
+                'cube':'bbbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have exactly 6 unique chars, and 9 of each unique chars') 
+    
         
     #Number of unique values doesn't equal six
     #Does not pass\returns error msg
     def test_check_HasLessThanSixUniqueValues(self):
         parm = {'op':'check',
-                'cube':'bbbbbbbbbbbbbbbbgggggggggoooooooooyyyyyyyyyyyyyyyyyyyy'}
+                'cube':'bbbbbbbbbBBBBBBBBBgggggggggoooooooooyyyyyyyyyyyyyyyyyy'}
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
@@ -108,7 +180,6 @@ class CheckTest(TestCase):
     
      
     #Has more than 6 unique characters in string
-    #Does not pass\returns error msg 
     def test_check_HasMoreThanSixUniqueValues(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbvvbbbbbbbgggggggggopoooooooyyyyyyyyyyyyyyyyyyxx'}
@@ -116,9 +187,8 @@ class CheckTest(TestCase):
         self.assertIn('status', result)
         status = result.get('status', None)
         self.assertEqual(status, 'error: cube requires less unique chars (exactly 6 are required)')
-        
-
-    
+      
+    #Tests for symbols not permitted (recall that a-z, A-Z, and 0-9 are permitted)
     def test_check_ContainsForrbidenSymbols(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbb!!!!!!!!!eeeeeeeeerrrrrrrrrtttttttttyyyyyyyyy'}
@@ -127,7 +197,63 @@ class CheckTest(TestCase):
         status = result.get('status', None)
         self.assertEqual(status, 'error: cube cannot contain chars including:`~!@#$%^&*()-_=+[]{}\|";:?/.>,<\'')
     
+    #checks first face center with other center pieces
+    def test_check_01_HasEqualCenterValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbrbrrrrbrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must 6 unique centers') 
+        
+    #checks 2nd face center with other center pieces
+    def test_check_02_HasEqualCenterValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrgrggggrggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must 6 unique centers') 
+     
+    #checks 3rd face center with other center pieces   
+    def test_check_03_HasEqualCenterValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggogoooogooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must 6 unique centers') 
+    
+    #checks fourth face center with other center pieces
+    def test_check_04_HasEqualCenterValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggooooooyooyyyyoyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must 6 unique centers') 
+    
+    #checks 5th face center with other center pieces
+    def test_check_05_HasEqualCenterValues(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbwbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwbwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must 6 unique centers')  
+      
+    #Test for when cube is not a str value 
+    def test_check_IsntString(self):
+        parm = {'op':'check',
+                'cube': 2 }
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must be a String')   
+    
+
         
         
+     
         
            

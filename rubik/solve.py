604806f58b501d7087810f9e0e1ccabee5895133
Shane <Shane@DESKTOP-Q1DOOLC.mshome.net>
2022-02-17 16:53:28 -0600

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -49,8 +49,15 @@ def _solve(parms):
             #updates encodedCube for subsequent calls for rotations
             encodedCube = rot
         
-            
-    
+        if(letter == 'F'):   
+            #performs F rotation
+            rot = front(encodedCube)
+            ##sets dict after being rotated
+            parms['cube'] = rot
+            #sets status to ok
+            parms['status'] = 'ok'
+            #updates encodedCube for subsequent calls for rotations
+            encodedCube = rot
     
     
     
@@ -159,6 +166,54 @@ def rightPrime(var):
     updatedCube[47] = tempArray[2] 
     updatedCube[50] = tempArray[5]   
     updatedCube[53] = tempArray[8]   
+
+    listToString = ''.join(updatedCube)
+    return listToString
+
+
+def front(var):
+    
+    updatedCube = list(var)
+    #makes empty array with 54 positions
+    tempArray = [None] * 54
+
+    #populates array with elements from cubeList
+    for i in range(0, 54):
+        tempArray[i] = updatedCube[i]
+        
+    #face 1  BLUE CENTER
+    updatedCube[0] = tempArray[6] 
+    updatedCube[1] = tempArray[3] 
+    updatedCube[2] = tempArray[0] 
+    updatedCube[3] = tempArray[7]
+    updatedCube[4] = tempArray[4]
+    updatedCube[5] = tempArray[1]
+    updatedCube[6] = tempArray[8]
+    updatedCube[7] = tempArray[5]
+    updatedCube[8] = tempArray[2]
+
+    #face 2 RED 
+    updatedCube[9] = tempArray[42]
+    updatedCube[12] = tempArray[43]
+    updatedCube[15] = tempArray[44]
+
+    #face 3 GREEN No change
+
+    #face 4 ORANGE CENTER ROTATE
+    updatedCube[29] = tempArray[45]
+    updatedCube[32] = tempArray[46]
+    updatedCube[35] = tempArray[47]
+
+    #face 5 YELLOW CENTER
+    updatedCube[42] = tempArray[35] 
+    updatedCube[43] = tempArray[32] 
+    updatedCube[44] = tempArray[29]
+
+    #face 6 WHITE CENTER
+    updatedCube[45] = tempArray[15] 
+    updatedCube[46] = tempArray[12]   
+    updatedCube[47] = tempArray[9]   
+    
 
     listToString = ''.join(updatedCube)
     return listToString

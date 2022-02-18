0198508b4724601569b700d53aa695cd0943de63
Shane <Shane@DESKTOP-Q1DOOLC.mshome.net>
2022-02-17 19:26:57 -0600

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -12,20 +12,7 @@ import rubik.check as check
 
 
 class Test(unittest.TestCase):
-
-
-    def test_solve_ShouldRotateValidNominalCube_Right(self):
-        parms = {'op':'solve',
-                 'rotate':'R',
-                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
-        }
-        updatedParms = {
-                'cube':'bbwbbwbbwrrrrrrrrryggyggyggoooooooooyybyybyybwwgwwgwwg',
-                'status':'ok'
-        }
-        encodedCube = solve._solve(parms)
-        self.assertEqual(encodedCube, updatedParms)
-        
+   
     
     def test_solve_ShouldRotateValidNominalCube_Right_ExampleFromSlide(self):
         parms = {'op':'solve',
@@ -38,46 +25,8 @@ class Test(unittest.TestCase):
         }
         encodedCube = solve._solve(parms)
         self.assertEqual(encodedCube, updatedParms)
-    
-    
-    
-    def test_solve_ShouldRotateValidNominalCube_Right_Twice(self):
-        parms = {'op':'solve',
-                 'rotate':'RR',
-                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        }
-        updatedParms = {
-                'cube':'ggbggbggbrrrrrrrrrgbbgbbgbbooooooooowwywwywwyyywyywyyw',
-                'status':'ok'
-        }
-        encodedCube = solve._solve(parms)
-        self.assertEqual(encodedCube, updatedParms)
         
-    #Rotate contains letter for rotation that isn't recoginized   
-    def test_solve_ErrorMsgForInvalidRotation(self):
-        parms = {'op':'solve',
-                 'rotate':'z',
-                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        }
-        updatedParms = {
-                'status':'error: invalid rotation'
-        }
-        encodedCube = solve._solve(parms)
-        self.assertEqual(encodedCube, updatedParms)
-        
-    #Rotate contains letter for rotation that isn't recoginized   
-    def test_solve_ErrorMsgForInvalidRotationMultiple(self):
-        parms = {'op':'solve',
-                 'rotate':'Rzr',
-                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        }
-        updatedParms = {
-                'status':'error: invalid rotation'
-        }
-        encodedCube = solve._solve(parms)
-        self.assertEqual(encodedCube, updatedParms)
-        
-        
+            
     def test_solve_ShouldRotateValidNominalCube_RightPrime(self):
         parms = {'op':'solve',
                  'rotate':'r',
@@ -215,6 +164,55 @@ class Test(unittest.TestCase):
                 'status':'ok'
         }
         encodedCube = solve._solve(parms)
-        self.assertEqual(encodedCube, updatedParms)    
+        self.assertEqual(encodedCube, updatedParms) 
+        
+        
+        def test_solve_ShouldRotateValidNominalCube_Right_Twice(self):
+        parms = {'op':'solve',
+                 'rotate':'RR',
+                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        }
+        updatedParms = {
+                'cube':'ggbggbggbrrrrrrrrrgbbgbbgbbooooooooowwywwywwyyywyywyyw',
+                'status':'ok'
+        }
+        encodedCube = solve._solve(parms)
+        self.assertEqual(encodedCube, updatedParms)
+        
+    #Rotate contains letter for rotation that isn't recoginized   
+    def test_solve_ErrorMsgForInvalidRotation(self):
+        parms = {'op':'solve',
+                 'rotate':'z',
+                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        }
+        updatedParms = {
+                'status':'error: invalid rotation'
+        }
+        encodedCube = solve._solve(parms)
+        self.assertEqual(encodedCube, updatedParms)
+        
+    #Rotate contains letter for rotation that isn't recoginized   
+    def test_solve_ErrorMsgForInvalidRotationMultiple(self):
+        parms = {'op':'solve',
+                 'rotate':'Rzr',
+                'cube':'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        }
+        updatedParms = {
+                'status':'error: invalid rotation'
+        }
+        encodedCube = solve._solve(parms)
+        self.assertEqual(encodedCube, updatedParms)
+        
+        def test_solve_ShouldRotateValidNominalCube_Right(self):
+        parms = {'op':'solve',
+                 'rotate':'R',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
+        }
+        updatedParms = {
+                'cube':'bbwbbwbbwrrrrrrrrryggyggyggoooooooooyybyybyybwwgwwgwwg',
+                'status':'ok'
+        }
+        encodedCube = solve._solve(parms)
+        self.assertEqual(encodedCube, updatedParms)   
     
     

0b40d596ec85205ddb2b7dca071910fd6fe86ee0
Shane <Shane@DESKTOP-Q1DOOLC>
2022-02-18 15:41:26 -0600

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -34,11 +34,19 @@ def _solve(parms):
     #for statement iterates through Rotation letters one at a time
     #and performs the necessary moves
     for letter in rotation:
+        
+        
+        if(rotation == None):   
+            rot = front(encodedCube)
+            parms['cube'] = rot
+            parms['status'] = 'ok'
+            encodedCube = rot
+            continue
     
         if (letter not in allowedLettersForRotation):
             parms['status']= 'error: invalid rotation'
             del parms['cube']
-            continue
+            
         
         if(letter == 'R'):
             #performs R rotation

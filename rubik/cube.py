ca0e53e80fe8b05f928a2cfb3d3ddbc4b7250dab
Shane <Shane@DESKTOP-Q1DOOLC>
2022-02-20 11:57:52 -0600

    Red Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -2,9 +2,7 @@ from rubik.check import *
 
 class Cube:
 
-    
-    
-    
+     
     
 # methods: instantiate:  inputs no input, out empty instance of cube
 #          load : takes serialized string and loads it into the cube
@@ -15,32 +13,27 @@ class Cube:
 #Rotate cube
 #Extract cube
 
-    #empty cube, constructor in python essentially
-    def __init__(self, cubeStr):   
-        # input parameter: None
-        # Output parameter: empty instance of cube
-        self.cube = cube
-        
-        #self.rotation = rotation
+    # #empty cube, constructor in python essentially
+    # def __init__(self, cubeStr):   
+    #     # input parameter: None
+    #     # Output parameter: empty instance of cube
+    #     self.cube = cube
+    #
+    #     #self.rotation = rotation
         
 
         
     
-    def load(self, cube):
-        result = check._check(parms)
-        
-        if(result == 'ok'):
-            
-            self.cube = parms.get('cube', None)
-
-            return cubeStr
-        
-        else: return "error"
-    
-    
-    
-
-    
+    # def load(self, cube):
+    #     result = check._check(parms)
+    #
+    #     if(result == 'ok'):
+    #
+    #         self.cube = parms.get('cube', None)
+    #
+    #         return cubeStr
+    #
+    #     else: return "error"
     #
     #
     #
@@ -52,6 +45,47 @@ class Cube:
     
     
     
+    def right(var):
+    
+        updatedCube = list(var)
+        #makes empty array with 54 positions
+        tempArray = [None] * 54
+    
+        #populates array with elements from cubeList
+        for i in range(0, 54):
+            tempArray[i] = updatedCube[i]
+        #face 1  BLUE CENTER
+        updatedCube[2] = tempArray[47]
+        updatedCube[5] = tempArray[50]
+        updatedCube[8] = tempArray[53]
+        #face 2 RED CENTER ROTATE
+        #face 2 RED CENTER
+        updatedCube[9] =  tempArray[15]
+        updatedCube[10] = tempArray[12]
+        updatedCube[11] = tempArray[9]
+        updatedCube[12] = tempArray[16]
+        updatedCube[13] = tempArray[13] #center doesn't change
+        updatedCube[14] = tempArray[10]
+        updatedCube[15] = tempArray[17]
+        updatedCube[16] = tempArray[14]
+        updatedCube[17] = tempArray[11]
+        #face3 GREEN CENTER
+        updatedCube[18] = tempArray[44]
+        updatedCube[21] = tempArray[41]
+        updatedCube[24] = tempArray[38]
+        #face 4 ORANGE CENTER (no change)
+        #face 5 YELLOW CENTER
+        updatedCube[38] = tempArray[2] 
+        updatedCube[41] = tempArray[5] 
+        updatedCube[44] = tempArray[8]
+    
+        #face 6 WHITE CENTER
+        updatedCube[47] = tempArray[24] 
+        updatedCube[50] = tempArray[21]   
+        updatedCube[53] = tempArray[18]  
+    
+        listToString = ''.join(updatedCube)
+        return listToString
     
     
     

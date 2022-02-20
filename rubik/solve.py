073cd8cd19557aebe6e73a9e8ec692233e89800e
Shane <Shane@DESKTOP-Q1DOOLC.mshome.net>
2022-02-19 18:29:39 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -35,7 +35,7 @@ def _solve(parms):
     #NOTE: if letter is found empty, do F turn 
     if(rotation =='' or rotation == None or 'rotate' not in parms and checkReturnsOkCube == True):
                
-        if(encodedCube != "" and len(encodedCube) == 54 and str(encodedCube)==true):
+        if(encodedCube != "" and len(encodedCube) == 54 and str(encodedCube)== True):
             rot = front(encodedCube)
         parms['cube'] = rot
         parms['status'] = 'ok'

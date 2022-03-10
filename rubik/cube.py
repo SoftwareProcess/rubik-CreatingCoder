e337477926c6f9eb99f4aa3a41cfcad0effb4e3b
Shane <Shane@host.docker.internal>
2022-03-10 07:02:46 -0600

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -151,7 +151,53 @@ class Cube:
     
     
     
-
+    @staticmethod
+    def rightPrime(self):
+          
+        updatedCube = list(self)
+        #makes empty array with 54 positions
+        tempArray = [None] * 54
+      
+        #populates array with elements from cubeList
+        for i in range(0, 54):
+            tempArray[i] = updatedCube[i]
+              
+        updatedCube[2] = tempArray[38]
+        updatedCube[5] = tempArray[41]
+        updatedCube[8] = tempArray[44]
+      
+        #face 2 RED CENTER ROTATE
+        #face 2 RED CENTER
+        updatedCube[9] =  tempArray[11]
+        updatedCube[10] = tempArray[14]
+        updatedCube[11] = tempArray[17]
+        updatedCube[12] = tempArray[10]
+        updatedCube[13] = tempArray[13] #center doesnt change
+        updatedCube[14] = tempArray[16]
+        updatedCube[15] = tempArray[9]
+        updatedCube[16] = tempArray[12]
+        updatedCube[17] = tempArray[15]
+      
+        #face3 GREEN CENTER
+        updatedCube[18] = tempArray[53]
+        updatedCube[21] = tempArray[50]
+        updatedCube[24] = tempArray[47]
+      
+        #face 4 ORANGE CENTER (no change)
+      
+        #face 5 YELLOW CENTER
+        updatedCube[38] = tempArray[24] 
+        updatedCube[41] = tempArray[21] 
+        updatedCube[44] = tempArray[18] 
+      
+        #face 6 WHITE CENTER
+        updatedCube[47] = tempArray[2] 
+        updatedCube[50] = tempArray[5]   
+        updatedCube[53] = tempArray[8]   
+      
+        listToString = ''.join(updatedCube)
+        return listToString
+      
         
  
    

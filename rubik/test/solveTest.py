0b3fa80b498f43ade2a8ed0293f70b84dd461790
Shane <Shane@host.docker.internal>
2022-03-11 14:05:39 -0600

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -902,7 +902,7 @@ class Test(unittest.TestCase):
         self.assertEqual(cubeList[41], cubeList[49])
         self.assertEqual(cubeList[43], cubeList[49])
         
-        self.assertEqual(parm['solution'], 'UUUDFlf')
+        self.assertEqual(parm['solution'], 'FFRRlUUUBDbLL')
     
     
     

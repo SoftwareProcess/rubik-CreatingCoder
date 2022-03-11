f31a41893615c2a3355de0b71cec58d8d94b8c0f
Shane <Shane@host.docker.internal>
2022-03-11 13:33:12 -0600

    Red Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -828,6 +828,21 @@ class Test(unittest.TestCase):
         self.assertEqual(cubeList[43], cubeList[49])
         self.assertEqual(parm['solution'], 'UUUDLbl') 
         
+        
+    def test_shouldReturnWhiteCross_WhiteOnTop_WhiteOnOrangeFaceAtPos28(self):
+        parm = {'op':'solve',
+                'cube': 'gbgybywrywobgrobgbwgobgoybwywrgoroyggwrrywyworboowrbyr'}
+            
+        encodedCube = solve._solve(parm)
+        #self.assertEqual(encodedCube, updatedParms)
+        cubeString = encodedCube.get('cube')
+        cubeList = list(cubeString)
+        self.assertEqual(cubeList[37], cubeList[49])
+        self.assertEqual(cubeList[39], cubeList[49])
+        self.assertEqual(cubeList[41], cubeList[49])
+        self.assertEqual(cubeList[43], cubeList[49])
+        self.assertEqual(parm['solution'], 'lBDbLL')
+        
     
     
     
